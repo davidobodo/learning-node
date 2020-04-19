5f421ae9e5f1e7ffb8602a9caada3176f35b87df
David Obodo <obododavid5@gmail.com>
2020-04-19 08:02:12 +0100

    parsing request paths

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function (req, res) {
+
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/*|\/*$/g, '');
+    console.log('Request received on path:' + trimmedPath)
     res.end('Hello world\n');
 });
 
