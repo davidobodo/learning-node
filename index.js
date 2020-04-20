caecc6e92fa9c8a3f4f508d8096cab36e604514e
David Obodo <obododavid5@gmail.com>
2020-04-20 17:13:21 +0100

    getting the http method

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ var url = require('url');
 var server = http.createServer(function (req, res) {
 
     var parsedUrl = url.parse(req.url, true);
+
     var path = parsedUrl.pathname;
+
     var trimmedPath = path.replace(/^\/*|\/*$/g, '');
-    console.log('Request received on path:' + trimmedPath)
+
+    var method = req.method.toLowerCase();
+
+    console.log('Request received on path:' + trimmedPath + ' with this method: ' + method)
+
     res.end('Hello world\n');
 });
 
