072fe6f61903145a1fa26777b66064acbdbbc0af
David Obodo <obododavid5@gmail.com>
2020-04-23 05:03:19 +0100

    implement logic for two servers

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
+var httpServer = http.createServer(function (req, res) {
     unifiedserver(req, res)
 });
 
-server.listen(config.port, function () {
-    console.log("The server is listening on port " + config.port + " now " + config.envName + " mode")
+httpServer.listen(config.httpPort, function () {
+    console.log("The server is listening on port " + config.httpPort + " now " + config.envName + " mode")
+})
+
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+}
+
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+    unifiedserver(req, res)
+});
+
+httpsServer.listen(config.httpsPort, function () {
+    console.log("The server is listening on port " + config.httpsPort + " now " + config.envName + " mode")
 })
 
 var unifiedserver = function (req, res) {
