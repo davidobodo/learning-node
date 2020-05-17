c17cc68b7eb5cf055f8eef3b1995f87aabd57130
David Obodo <obododavid5@gmail.com>
2020-05-17 19:29:38 +0100

    add logic(js) form signup form

--- public/app.js
+++ public/app.js
@@ -112,156 +112,156 @@ app.client.request = function (headers, path, method, queryStringObject, payload
 // };
 
 // // Bind the forms
-// app.bindForms = function () {
-//     if (document.querySelector("form")) {
+app.bindForms = function () {
+    if (document.querySelector("form")) {
 
-//         var allForms = document.querySelectorAll("form");
-//         for (var i = 0; i < allForms.length; i++) {
-//             allForms[i].addEventListener("submit", function (e) {
+        var allForms = document.querySelectorAll("form");
+        for (var i = 0; i < allForms.length; i++) {
+            allForms[i].addEventListener("submit", function (e) {
 
-//                 // Stop it from submitting
-//                 e.preventDefault();
-//                 var formId = this.id;
-//                 var path = this.action;
-//                 var method = this.method.toUpperCase();
+                // Stop it from submitting
+                e.preventDefault();
+                var formId = this.id;
+                var path = this.action;
+                var method = this.method.toUpperCase();
 
-//                 // Hide the error message (if it's currently shown due to a previous error)
-//                 document.querySelector("#" + formId + " .formError").style.display = 'none';
+                // Hide the error message (if it's currently shown due to a previous error)
+                document.querySelector("#" + formId + " .formError").style.display = 'none';
 
-//                 // Hide the success message (if it's currently shown due to a previous error)
-//                 if (document.querySelector("#" + formId + " .formSuccess")) {
-//                     document.querySelector("#" + formId + " .formSuccess").style.display = 'none';
-//                 }
-
-
-//                 // Turn the inputs into a payload
-//                 var payload = {};
-//                 var elements = this.elements;
-//                 for (var i = 0; i < elements.length; i++) {
-//                     if (elements[i].type !== 'submit') {
-//                         // Determine class of element and set value accordingly
-//                         var classOfElement = typeof (elements[i].classList.value) == 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
-//                         var valueOfElement = elements[i].type == 'checkbox' && classOfElement.indexOf('multiselect') == -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
-//                         var elementIsChecked = elements[i].checked;
-//                         // Override the method of the form if the input's name is _method
-//                         var nameOfElement = elements[i].name;
-//                         if (nameOfElement == '_method') {
-//                             method = valueOfElement;
-//                         } else {
-//                             // Create an payload field named "method" if the elements name is actually httpmethod
-//                             if (nameOfElement == 'httpmethod') {
-//                                 nameOfElement = 'method';
-//                             }
-//                             // Create an payload field named "id" if the elements name is actually uid
-//                             if (nameOfElement == 'uid') {
-//                                 nameOfElement = 'id';
-//                             }
-//                             // If the element has the class "multiselect" add its value(s) as array elements
-//                             if (classOfElement.indexOf('multiselect') > -1) {
-//                                 if (elementIsChecked) {
-//                                     payload[nameOfElement] = typeof (payload[nameOfElement]) == 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
-//                                     payload[nameOfElement].push(valueOfElement);
-//                                 }
-//                             } else {
-//                                 payload[nameOfElement] = valueOfElement;
-//                             }
-
-//                         }
-//                     }
-//                 }
+                // Hide the success message (if it's currently shown due to a previous error)
+                if (document.querySelector("#" + formId + " .formSuccess")) {
+                    document.querySelector("#" + formId + " .formSuccess").style.display = 'none';
+                }
 
 
-//                 // If the method is DELETE, the payload should be a queryStringObject instead
-//                 var queryStringObject = method == 'DELETE' ? payload : {};
+                // Turn the inputs into a payload
+                var payload = {};
+                var elements = this.elements;
+                for (var i = 0; i < elements.length; i++) {
+                    if (elements[i].type !== 'submit') {
+                        // Determine class of element and set value accordingly
+                        var classOfElement = typeof (elements[i].classList.value) == 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
+                        var valueOfElement = elements[i].type == 'checkbox' && classOfElement.indexOf('multiselect') == -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
+                        var elementIsChecked = elements[i].checked;
+                        // Override the method of the form if the input's name is _method
+                        var nameOfElement = elements[i].name;
+                        if (nameOfElement == '_method') {
+                            method = valueOfElement;
+                        } else {
+                            // Create an payload field named "method" if the elements name is actually httpmethod
+                            if (nameOfElement == 'httpmethod') {
+                                nameOfElement = 'method';
+                            }
+                            // Create an payload field named "id" if the elements name is actually uid
+                            if (nameOfElement == 'uid') {
+                                nameOfElement = 'id';
+                            }
+                            // If the element has the class "multiselect" add its value(s) as array elements
+                            if (classOfElement.indexOf('multiselect') > -1) {
+                                if (elementIsChecked) {
+                                    payload[nameOfElement] = typeof (payload[nameOfElement]) == 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
+                                    payload[nameOfElement].push(valueOfElement);
+                                }
+                            } else {
+                                payload[nameOfElement] = valueOfElement;
+                            }
 
-//                 // Call the API
-//                 app.client.request(undefined, path, method, queryStringObject, payload, function (statusCode, responsePayload) {
-//                     // Display an error on the form if needed
-//                     if (statusCode !== 200) {
+                        }
+                    }
+                }
 
-//                         if (statusCode == 403) {
-//                             // log the user out
-//                             app.logUserOut();
 
-//                         } else {
+                // If the method is DELETE, the payload should be a queryStringObject instead
+                var queryStringObject = method == 'DELETE' ? payload : {};
 
-//                             // Try to get the error from the api, or set a default error message
-//                             var error = typeof (responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+                // Call the API
+                app.client.request(undefined, path, method, queryStringObject, payload, function (statusCode, responsePayload) {
+                    // Display an error on the form if needed
+                    if (statusCode !== 200) {
 
-//                             // Set the formError field with the error text
-//                             document.querySelector("#" + formId + " .formError").innerHTML = error;
+                        if (statusCode == 403) {
+                            // log the user out
+                            app.logUserOut();
 
-//                             // Show (unhide) the form error field on the form
-//                             document.querySelector("#" + formId + " .formError").style.display = 'block';
-//                         }
-//                     } else {
-//                         // If successful, send to form response processor
-//                         app.formResponseProcessor(formId, payload, responsePayload);
-//                     }
+                        } else {
 
-//                 });
-//             });
-//         }
-//     }
-// };
+                            // Try to get the error from the api, or set a default error message
+                            var error = typeof (responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
 
-// // Form response processor
-// app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
-//     var functionToCall = false;
-//     // If account creation was successful, try to immediately log the user in
-//     if (formId == 'accountCreate') {
-//         // Take the phone and password, and use it to log the user in
-//         var newPayload = {
-//             'phone': requestPayload.phone,
-//             'password': requestPayload.password
-//         };
+                            // Set the formError field with the error text
+                            document.querySelector("#" + formId + " .formError").innerHTML = error;
 
-//         app.client.request(undefined, 'api/tokens', 'POST', undefined, newPayload, function (newStatusCode, newResponsePayload) {
-//             // Display an error on the form if needed
-//             if (newStatusCode !== 200) {
+                            // Show (unhide) the form error field on the form
+                            document.querySelector("#" + formId + " .formError").style.display = 'block';
+                        }
+                    } else {
+                        // If successful, send to form response processor
+                        app.formResponseProcessor(formId, payload, responsePayload);
+                    }
 
-//                 // Set the formError field with the error text
-//                 document.querySelector("#" + formId + " .formError").innerHTML = 'Sorry, an error has occured. Please try again.';
+                });
+            });
+        }
+    }
+};
 
-//                 // Show (unhide) the form error field on the form
-//                 document.querySelector("#" + formId + " .formError").style.display = 'block';
+// Form response processor
+app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
+    var functionToCall = false;
+    // If account creation was successful, try to immediately log the user in
+    if (formId == 'accountCreate') {
+        // Take the phone and password, and use it to log the user in
+        var newPayload = {
+            'phone': requestPayload.phone,
+            'password': requestPayload.password
+        };
 
-//             } else {
-//                 // If successful, set the token and redirect the user
-//                 app.setSessionToken(newResponsePayload);
-//                 window.location = '/checks/all';
-//             }
-//         });
-//     }
-//     // If login was successful, set the token in localstorage and redirect the user
-//     if (formId == 'sessionCreate') {
-//         app.setSessionToken(responsePayload);
-//         window.location = '/checks/all';
-//     }
+        app.client.request(undefined, 'api/tokens', 'POST', undefined, newPayload, function (newStatusCode, newResponsePayload) {
+            // Display an error on the form if needed
+            if (newStatusCode !== 200) {
 
-//     // If forms saved successfully and they have success messages, show them
-//     var formsWithSuccessMessages = ['accountEdit1', 'accountEdit2', 'checksEdit1'];
-//     if (formsWithSuccessMessages.indexOf(formId) > -1) {
-//         document.querySelector("#" + formId + " .formSuccess").style.display = 'block';
-//     }
+                // Set the formError field with the error text
+                document.querySelector("#" + formId + " .formError").innerHTML = 'Sorry, an error has occured. Please try again.';
 
-//     // If the user just deleted their account, redirect them to the account-delete page
-//     if (formId == 'accountEdit3') {
-//         app.logUserOut(false);
-//         window.location = '/account/deleted';
-//     }
+                // Show (unhide) the form error field on the form
+                document.querySelector("#" + formId + " .formError").style.display = 'block';
 
-//     // If the user just created a new check successfully, redirect back to the dashboard
-//     if (formId == 'checksCreate') {
-//         window.location = '/checks/all';
-//     }
+            } else {
+                // If successful, set the token and redirect the user
+                app.setSessionToken(newResponsePayload);
+                window.location = '/checks/all';
+            }
+        });
+    }
+    // If login was successful, set the token in localstorage and redirect the user
+    if (formId == 'sessionCreate') {
+        app.setSessionToken(responsePayload);
+        window.location = '/checks/all';
+    }
 
-//     // If the user just deleted a check, redirect them to the dashboard
-//     if (formId == 'checksEdit2') {
-//         window.location = '/checks/all';
-//     }
+    // If forms saved successfully and they have success messages, show them
+    var formsWithSuccessMessages = ['accountEdit1', 'accountEdit2', 'checksEdit1'];
+    if (formsWithSuccessMessages.indexOf(formId) > -1) {
+        document.querySelector("#" + formId + " .formSuccess").style.display = 'block';
+    }
 
-// };
+    // If the user just deleted their account, redirect them to the account-delete page
+    if (formId == 'accountEdit3') {
+        app.logUserOut(false);
+        window.location = '/account/deleted';
+    }
+
+    // If the user just created a new check successfully, redirect back to the dashboard
+    if (formId == 'checksCreate') {
+        window.location = '/checks/all';
+    }
+
+    // If the user just deleted a check, redirect them to the dashboard
+    if (formId == 'checksEdit2') {
+        window.location = '/checks/all';
+    }
+
+};
 
 // // Get the session token from localstorage and set it in the app.config object
 // app.getSessionToken = function () {
@@ -519,7 +519,7 @@ app.client.request = function (headers, path, method, queryStringObject, payload
 app.init = function () {
 
     // Bind all form submissions
-    // app.bindForms();
+    app.bindForms();
 
     // Bind logout logout button
     // app.bindLogoutButton();
